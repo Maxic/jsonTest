bd9a5ab2f3495ba5fab093c57681ee030f93c2c7
Maxic <barendez@gmail.com>
2018-01-07 13:44:16 +0100

    Added procfile

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, Response
+from datetime import datetime
+import json
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400">
+    """.format(time=the_time)
+
+@app.route("/slackTest", methods=['POST'])
+def receiveSlackMessages():
+    data = request.get_json()
+    if (data is not None):
+        js = json.dumps(data)
+        print("Json: " + js)
+    else:
+        print("Received request, but no data")
+    return Response(data, status=200, mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
